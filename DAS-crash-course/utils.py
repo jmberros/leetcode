b4ed7b3136472bd9cb1e39c98838b8a01bbdccd4
Juan Manuel Berros <juanmaberros@gmail.com>
2023-08-04 10:52:10 -0300

    hashing example problems

---
+++ DAS-crash-course/utils.py
@@ -0,0 +1,18 @@
+def run_test_cases(Solution, test_cases):
+    methods = [
+        attribute for attribute in dir(Solution)
+        if not attribute.startswith("__")
+        and callable(getattr(Solution, attribute))
+    ]
+
+    for method_name in methods:
+        print(f"~~ Testing {method_name} ~~")
+        method = getattr(Solution(), method_name)
+
+        for *in_, exp in test_cases:
+            in_ = tuple(in_)
+            print(f"{in_ = }")
+            out = method(**in_[0]) if isinstance(in_[0], dict) else method(*in_)
+            print(f"{out = }")
+            print(f"{exp = }")
+            print("--")
