d3decccfdb59b4c9490b0a4d7c72012ab079b9e1
Juan Manuel Berros <juanmaberros@gmail.com>
2023-08-04 19:56:21 -0300

    wip chapter 2

--- DAS-crash-course/utils.py
+++ DAS-crash-course/utils.py
@@ -1,3 +1,6 @@
+from termcolor import colored
+
+
 def run_test_cases(Solution, test_cases):
     methods = [
         attribute for attribute in dir(Solution)
@@ -13,6 +16,10 @@ def run_test_cases(Solution, test_cases):
             in_ = tuple(in_)
             print(f"{in_ = }")
             out = method(**in_[0]) if isinstance(in_[0], dict) else method(*in_)
-            print(f"{out = }")
+            if out != exp:
+                out = colored(str(out), "red")
+            else:
+                out = colored(str(out), "green")
+            print("out = " + out)
             print(f"{exp = }")
             print("--")
