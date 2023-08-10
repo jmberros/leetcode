df959826c75cd8a4b2677a94df85efb91857e1c4
Juan Manuel Berros <juanmaberros@gmail.com>
2023-08-10 16:06:59 -0300

    wip 4

---
+++ DAS-crash-course/04-SQ-933.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/number-of-recent-calls/
+
+from utils import run_test_cases
+
+
+test_cases = [
+
+]
+
+
+class RecentCounter:
+
+    def __init__(self):
+        pass        
+
+    def ping(self, t: int) -> int:
+        pass        
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
+
+
+if __name__ == "__main__":
+    run_test_cases(RecentCounter, test_cases)
