fa8f1851a6ee5ab96ead2d4a7a78ef457a52cc62
Juan Manuel Berros <juanmaberros@gmail.com>
2023-08-13 20:26:21 -0300

    WIP 5

---
+++ DAS-crash-course/05-BTs-98.py
@@ -0,0 +1,38 @@
+# https://leetcode.com/problems/validate-binary-search-tree/description/
+
+from collections import deque
+from typing import Optional, List
+
+from binarytree import Node, build2
+from utils import run_test_cases
+
+
+test_cases = [
+    ([2, 1, 3], True),
+    ([5, 1, 4, None, None, 3, 6], False),
+    ([5, 4, 6, None, None, 3, 7], False),
+    ([2, 2, 2], False),
+]
+
+
+class Solution:
+    # Time: O(N)
+    # Space: O(N) worst case
+    def isValidBST(self, root) -> bool:
+        if isinstance(root, list):
+            root = build2(root)
+        print(root)
+
+        def helper(node, lb, ub):
+            # Test if current node is within valid limits or raise
+            if node.val <= lb or node.val >= ub:
+                return False
+            left_is_valid = (node.left is None) or helper(node.left, lb=lb, ub=node.val)
+            right_is_valid = (node.right is None) or helper(node.right, lb=node.val, ub=ub)
+            return left_is_valid and right_is_valid
+
+        return helper(root, float("-inf"), float("+inf"))
+
+
+if __name__ == "__main__":
+    run_test_cases(Solution, test_cases)
